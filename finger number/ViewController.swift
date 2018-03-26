9d7bbfd0a44aadb76ed65df46e2e327f7067c563
csd5766 <csd5766@naver.com>
2018-03-26 15:41:42 +0900

    commit

--- finger number/ViewController.swift
+++ finger number/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func Guess(_ sender: Any) {
+        //랜덤 숫자 생성
+        let number = arc4random() % 6
+        print (number)
+        let numberString = String(number)
+        
+        if input.text == numberString {
+            resultLabel.text = "정답 딩동댕"
+        } else {
+            resultLabel.text = "틀림 땡 정답은\(number)"
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
